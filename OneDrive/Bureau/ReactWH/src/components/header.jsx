9304c3db1ef0491ed343763651c93af4314feb0d
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-04-08 14:49:08 +0200

    activite4

---
+++ OneDrive/Bureau/ReactWH/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FunctionHeader = () => {
+    return (
+        <header>
+            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Un mexicain qui joue de la guitare" />
+            <nav>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="/">Page 2</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default FunctionHeader;
