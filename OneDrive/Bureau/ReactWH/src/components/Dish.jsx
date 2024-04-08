b27b8f92a6ad175c173e1c40bb475bc8fbd8b819
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-04-08 16:01:09 +0200

    activite6 correction v3

--- OneDrive/Bureau/ReactWH/src/components/Dish.jsx
+++ OneDrive/Bureau/ReactWH/src/components/Dish.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const Dish = ({name , img , prix, description}) => {
     return (
-        <main>
+        <div>
             <img src={img}/>
             <h1>{name}</h1>
-            <p>{description}</p>
             <h4>{prix}€</h4>
-        </main>
+        </div>
     )
 }
 
