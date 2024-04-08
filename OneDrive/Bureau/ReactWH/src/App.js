a78b62e424a4e1f74fc1f056e45a7bc82126da19
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-04-08 15:57:29 +0200

    activite6 correctionV2

--- OneDrive/Bureau/ReactWH/src/App.js
+++ OneDrive/Bureau/ReactWH/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import Footer from './components/footer';
-import Header from './components/header';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import Dish from './components/Dish';
 
 function App() {
   return (
     <div>
       <Header/>
+      <main>
       <Dish name={'Tacos'}  />
       <Dish name={'Enchiladas'} prix={12} img={'https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg'} />
       <Dish name={'Mole poblano'} prix={15} img={' https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg'} />
+      </main>
       <Footer/>
     </div>
   );
