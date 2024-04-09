173e92eb9711ab2c2e3cb64754b8e44320748921
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-04-09 11:11:20 +0200

    act8first

--- OneDrive/Bureau/ReactWH/src/components/Header.jsx
+++ OneDrive/Bureau/ReactWH/src/components/Header.jsx
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "../assets/styles/header.css";
+import logo from "../assets/logo.webp";
 
 const Header = () => {
   return (
@@ -11,7 +12,7 @@ const Header = () => {
         <Navbar.Brand href="#home">
           <img
             alt="Mexicain qui joue de la guitare"
-            src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+            src={logo}
             className="d-inline-block align-top navbar-img"
           />
         </Navbar.Brand>
