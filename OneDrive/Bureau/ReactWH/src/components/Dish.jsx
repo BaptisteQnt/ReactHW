051397cb5212459ac6b66bceb908fb4ccba100be
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-04-10 12:43:53 +0200

    act16 ISNew

--- OneDrive/Bureau/ReactWH/src/components/Dish.jsx
+++ OneDrive/Bureau/ReactWH/src/components/Dish.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 import Badge from 'react-bootstrap/Badge';
 import "../assets/styles/dish.css";
 
-const Dish = ({ name, img, price, slug, isNew }) => {
+const Dish = ({ name, img, price, slug, ISNew }) => {
   return (
     <Link to={"/plat/"+slug}>
       <Card>
         <Card.Img className="card-img" variant="top" src={img} />
-          {isNew === true && <Badge className="card-img-dish" bg="primary">Nouveau</Badge>}
+          {ISNew === true && <Badge className="card-img-dish" bg="primary">Nouveau</Badge>}
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>{price}</Card.Text>
