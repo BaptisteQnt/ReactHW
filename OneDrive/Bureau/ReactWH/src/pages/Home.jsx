f7c9ea5cc452b69f4892b5bdbca872e53838491b
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-04-10 10:24:48 +0200

    act15first

--- OneDrive/Bureau/ReactWH/src/pages/Home.jsx
+++ OneDrive/Bureau/ReactWH/src/pages/Home.jsx
@@ -5,39 +5,42 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const Home = () => {
+  const dishes = [
+    {
+      id: 1,
+      name: "Tacos",
+      price: 3,
+      img: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      slug: "tacos",
+    },
+    {
+      id: 2,
+      name: "Enchiladas",
+      price: 12,
+      img: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      slug: "enchiladas",
+    },
+    {
+      id: 3,
+      name: "Mole poblano",
+      price: 15,
+      img: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      slug: "mole-poblano",
+    },
+  ];
   return (
     <Container>
       <Row className="mt-5 mb-5">
-        <Col>
-          <Dish
-            name={"Tacos"}
-            price={3}
-            img={
-              "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-            }
-            slug={"tacos"}
-          />
-        </Col>
-        <Col>
-          <Dish
-            name={"Enchiladas"}
-            price={12}
-            img={
-              "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-            }
-            slug={"enchiladas"}
-          />
-        </Col>
-        <Col>
-          <Dish
-            name={"Mole poblano"}
-            price={15}
-            img={
-              " https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-            }
-            slug={"mole-poblano"}
-          />
-        </Col>
+        {dishes.map((dish) => (
+          <Col key={dish.id}>
+            <Dish
+              name={dish.name}
+              price={dish.price}
+              img={dish.img}
+              slug={dish.slug}
+            />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
