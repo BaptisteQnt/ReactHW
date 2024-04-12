2557d75820bc6ace4cf0fe7393b2994be4a7f3d8
BaptisteQnt <112851042+BaptisteQnt@users.noreply.github.com>
2024-04-12 10:21:48 +0200

    act23 dish name in title

--- OneDrive/Bureau/ReactWH/src/pages/DishDetails.jsx
+++ OneDrive/Bureau/ReactWH/src/pages/DishDetails.jsx
@@ -5,45 +5,52 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Dishes from "../datas/dishes.json";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { CartContext } from "../utils/context/CartContext";
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 
 const DishDetails = () => {
   const { slug } = useParams();
   const [dish, setDish] = useState(null);
   const navigate = useNavigate();
-  const { addToCart } = useContext(CartContext)
+  const { addToCart } = useContext(CartContext);
 
   useEffect(() => {
-    const currentDish = Dishes.find((dish) => dish.slug === slug );
-    if(!currentDish){
+    const currentDish = Dishes.find((dish) => dish.slug === slug);
+    if (!currentDish) {
       navigate("*");
     }
     setDish(currentDish);
-    
-  }, [slug, navigate])
-
+  }, [slug, navigate]);
 
   return (
     <Container className="mt-5 mb-5">
-      <Helmet>
-        <title>Détails du Plat</title>
-      </Helmet>
-      {dish ? <Row>
-        <Col>
-          <img className="mw-100" src= { dish.img } alt={dish.name} />
-        </Col>
-        <Col>
-          <h2>{ dish.name }</h2>
-          <p>{ dish.description }</p>
-          <p>{ dish.price }€</p>
-          <Button onClick={() => addToCart(dish)} variant="primary">Commander</Button>
-        </Col>
-      </Row> : (
+      {dish ? (
         <Row>
-          <h1>Chargement en cours ...</h1>
+          <Helmet>
+            <title>{dish.name}</title>
+          </Helmet>
+          <Col>
+            <img className="mw-100" src={dish.img} alt={dish.name} />
+          </Col>
+          <Col>
+            <h2>{dish.name}</h2>
+            <p>{dish.description}</p>
+            <p>{dish.price}€</p>
+            <Button onClick={() => addToCart(dish)} variant="primary">
+              Commander
+            </Button>
+          </Col>
         </Row>
+      ) : (
+        <div>
+          <Helmet>
+            <title>Chargement en cours ...</title>
+          </Helmet>
+          <Row>
+            <h1>Chargement en cours ...</h1>
+          </Row>
+        </div>
       )}
     </Container>
   );
